cf565d4f9a796d3749cf4e4e08d72b2103b936a4
Joe <josephlewismoulton@gmail.com>
2022-10-28 17:27:37 +0100

    Cart Created and logic starting to be implemented

--- src/App.js
+++ src/App.js
@@ -5,24 +5,35 @@ import Footer from "./components/Footer";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-import {BrowserRouter as Router , Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
-import {books} from "./data";
+import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart,book])
+  }
+
   return (
     <Router>
-       
-    <div className="App">
-      <Nav></Nav>
-      <Route path="/" exact component={Home}/>
-      <Route path="/books" exact render={() => <Books books={books}/>}/>
-      <Route path="/books/:id" render={() => <BookInfo books = {books}/>}/>
-      
-      <Footer></Footer>
-    </div>
+      <div className="App">
+        <Nav></Nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} cart={cart} />}
+        />
+        <Route path="/cart" render={() => <Cart books={books} cart={cart} />} />
+
+        <Footer></Footer>
+      </div>
     </Router>
   );
 }
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,13 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Book from "../components/ui/Book";
 import Price from "../components/ui/Price";
 import Rating from "../components/ui/Rating";
 
-export default function BookInfo({ books }) {
+export default function BookInfo({ books, addToCart, cart }) {
   const { id } = useParams();
   const book = books.find((book) => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find((book) => +book.id === +id);
+  }
+
   return (
     <div id="books__body">
       <main id="books__main">
@@ -51,7 +61,15 @@ export default function BookInfo({ books }) {
                     officia!
                   </p>
                 </div>
-                <button className="btn">Add to Cart</button>
+                {bookExistsOnCart() ? (
+                  <Link to ={`/cart`} className="book__link">
+                  <button className="btn">Checkout</button>
+                  </Link>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
+                    Add to Cart
+                  </button>
+                )}
               </div>
             </div>
           </div>
