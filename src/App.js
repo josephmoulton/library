86dcd09a4a7062226020c8bc578b7153f6260050
Joe <josephlewismoulton@gmail.com>
2022-10-28 16:41:06 +0100

    Book selector implemented

--- src/App.js
+++ src/App.js
@@ -9,14 +9,18 @@ import {BrowserRouter as Router , Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import {books} from "./data";
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
     <Router>
+       
     <div className="App">
       <Nav></Nav>
       <Route path="/" exact component={Home}/>
-      <Route path="/books" render={() => <Books books={books}/>}/>
+      <Route path="/books" exact render={() => <Books books={books}/>}/>
+      <Route path="/books/:id" render={() => <BookInfo books = {books}/>}/>
+      
       <Footer></Footer>
     </div>
     </Router>
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,35 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
+import Price from "./Price";
+import Rating from "./Rating";
 
 export default function Book({ book }) {
   return (
     <div className="book">
-      <a href="">
+      <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
           <img src={book.url} alt="" className="book__img" />
         </figure>
-      </a>
+      </Link>
       <div className={book.title}>
-        <a href="/" className="book__title--link">
+        <Link to={`/books/${book.id}`} className="book__title--link">
           Atomic Habits
-        </a>
+        </Link>
       </div>
-      <div className="book__ratings">
-        {
-            new Array(Math.floor(book.rating)).fill(0).map((_,index) => <FontAwesomeIcon icon="star" key = {index} />)
-        }
-        {
-            !Number.isInteger(book.rating) && <FontAwesomeIcon icon="star-half-alt"/>
-        }
-      </div>
-      <div className="book__price">
-        {book.salePrice ? (
-          <>
-            <span className="book__price--normal">£{book.originalPrice.toFixed(2)}</span>£
-            {book.salePrice.toFixed(2)}
-          </>
-        ) : `£${book.originalPrice.toFixed(2)}`}
-      </div>
+      <Rating rating={book.rating}></Rating>
+      <Price originalPrice={book.originalPrice} salePrice={book.salePrice}></Price>
     </div>
   );
 }
