c469f7c8350535275d7c888979bf25a9634874c7
Joe <josephlewismoulton@gmail.com>
2022-10-28 15:22:03 +0100

    Books pages added and footer completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Featured from "./components/Featured";
+import Footer from "./components/Footer";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
+import {BrowserRouter as Router , Route} from 'react-router-dom'
+import Home from "./pages/Home";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     <Nav></Nav>
-     <Landing></Landing>
-     <Highlights></Highlights>
+      <Nav></Nav>
+      <Route path="/" exact component={Home}/>
+
+      <Footer></Footer>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Books.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Books() {
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <section>
+          <div className="books__container">
+            <div className="row">
+              <div className="books__header">
+                <h2 className="section__title books__header--title">
+                  All Books
+                </h2>
+                <select  id="filter">
+                    <option value="" selected disabled>Sort</option>
+                    <option value="LOW_TO_HIGH">Price, Low to high</option>
+                    <option value="HIGH_TO_LOW">Price, High to low</option>
+                    <option value="RATING">Rating</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
